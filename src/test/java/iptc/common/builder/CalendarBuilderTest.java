7d5708d68f8c4a35c0615c2cbfbc26358033fa86
Ben Asmussen <ben.asmussen@gmail.com>
2015-04-04 20:55:30 +0200

    Refactor Testcase

--- src/test/java/iptc/common/builder/CalendarBuilderTest.java
+++ src/test/java/iptc/common/builder/CalendarBuilderTest.java
@@ -43,33 +43,33 @@ public class CalendarBuilderTest
     @Test
     public void testCalendarTimeZoneAndLocale() throws Exception
     {
-        Calendar cal = calendar().timzone("Europe/Berlin").locale(Locale.GERMAN).build();
+        Calendar cal = calendar().timzone("GMT").locale(Locale.GERMANY).build();
         assertNotNull(cal);
-        assertEquals("Europe/Berlin", cal.getTimeZone().getID());
+        assertEquals("GMT", cal.getTimeZone().getID());
     }
 
     @Test
     public void testDate() throws Exception
     {
-        Calendar cal = calendar().timzone("Europe/Berlin").locale(Locale.GERMAN).date("2015-01-01").build();
+        Calendar cal = calendar().timzone("GMT").locale(Locale.GERMANY).date("2015-01-01").build();
 
-        SimpleDateFormat sdf = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss Z");
+        SimpleDateFormat sdf = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss Z", Locale.GERMANY);
         sdf.setCalendar(cal);
         String format = sdf.format(cal.getTime());
 
-        assertEquals("Mo, 29 Dez 2014 00:00:00 +0100", format);
+        assertEquals("So, 28 Dez 2014 23:00:00 +0000", format);
     }
 
     @Test
     public void testDateAndTime() throws Exception
     {
-        Calendar cal = calendar().timzone("Europe/Berlin").locale(Locale.GERMAN).date("2015-01-01").time("06:00:00").build();
+        Calendar cal = calendar().timzone("GMT").locale(Locale.GERMANY).date("2015-01-01").time("06:00:00").build();
 
-        SimpleDateFormat sdf = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss Z");
+        SimpleDateFormat sdf = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss Z", Locale.GERMANY);
         sdf.setCalendar(cal);
         String format = sdf.format(cal.getTime());
 
-        assertEquals("Mo, 29 Dez 2014 06:00:00 +0100", format);
+        assertEquals("Mo, 29 Dez 2014 05:00:00 +0000", format);
     }
 
 }
